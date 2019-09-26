eb29630541826ae58df38b9daa2c0e373b237411
Eric Loudon <loudon.eric@gmail.com>
2019-09-26 09:05:04 +0000

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,6 +1,8 @@
 class CreateArtists < ActiveRecord::Migration[4.2]
+
+#instead of up or down
   def change
-    create_table :artists do |t|
+  create_table :artists do |t|
       t.string :name
       t.string :genre
       t.integer :age
